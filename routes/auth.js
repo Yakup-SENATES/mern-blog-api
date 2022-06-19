78ad18e10858abf6407fab42f3db9d5eb7661a30
CihanLoyan <cihanloyan@gmail.com>
2022-06-19 14:05:05 +0300

    Fix api

--- routes/auth.js
+++ routes/auth.js
@@ -1,47 +1,37 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-//Register
 
+//REGISTER
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       userName: req.body.userName,
       email: req.body.email,
-      password: hashedPassword,
+      password: hashedPass,
     });
 
     const user = await newUser.save();
     res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
-
-//Login
-
+//LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ userName: req.body.userName });
-    !user && res.status(404).json({ message: "User not found" });
+    !user && res.status(400).json("Kullanıcı bulunamadı!");
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    //if (!validPassword) {
-    //  return res.status(401).json({ message: "Invalid password" });
-    //}  aynı anlama gelen kod !!!
-
-    !validPassword && res.status(401).json({ message: "Invalid password" });
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Kullanıcı adı veya şifre!");
 
     const { password, ...others } = user._doc;
-
     res.status(200).json(others);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
