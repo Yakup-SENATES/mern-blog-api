d1b36782a8f41620abc8ff33bea643d47fd1b679
CihanLoyan <cihanloyan@gmail.com>
2022-06-21 21:45:38 +0300

    Fix put operation for post

--- routes/posts.js
+++ routes/posts.js
@@ -15,21 +15,26 @@ router.post("/", async (req, res) => {
 //UPDATE
 
 router.put("/:id", async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    return res.status(401).json({ message: "Not Authorized" });
-  } else {
-    try {
-      const updatePost = await Post.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatePost);
-    } catch (error) {
-      res.status(500).json(error);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can update only your post!");
     }
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
