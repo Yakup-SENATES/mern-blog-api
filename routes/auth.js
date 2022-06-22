ca3d4ee5ba49cbbb9b1a6c53fa48d9a3b4de0c52
Yakup SENATES <76456346+Yakup-SENATES@users.noreply.github.com>
2022-06-22 22:06:29 +0300

    mail

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-
+const Token = require("../models/Token");
+const crypto = require("crypto");
+const sendEmail = require("../utils/sendEmail");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -15,7 +17,16 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.status(200).json(user);
+    const token = await new Token({
+      userId: user._id,
+      token: crypto.randomBytes(32).toString("hex"),
+    }).save();
+    const url = `${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
+    await sendEmail(user.email, "Verify Email", url);
+    res
+      .status(200)
+      .send({ message: "An email sent to your mail. Please Verify..." })
+      .json(user);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -29,9 +40,43 @@ router.post("/login", async (req, res) => {
     const validated = await bcrypt.compare(req.body.password, user.password);
     !validated && res.status(400).json("Kullanıcı adı veya şifre!");
 
+    if (!user.verified) {
+      let token = await Token.findOne({ userId: user._id });
+      if (!token) {
+        token = await new Token({
+          userId: user._id,
+          token: crypto.randomBytes(32).toString("hex"),
+        }).save();
+        const url = `${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
+        await sendEmail(user.email, "Verify Email", url);
+      }
+      return res
+        .status(400)
+        .send({ message: "An email sent to your mail. Please Verify..." });
+    }
     const { password, ...others } = user._doc;
     res.status(200).json(others);
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Token Verification
+router.get("/id:/verify/:token", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user)
+      return res.status(400).send({ message: "Kullanıcı bulunamadı!" });
+
+    const token = await token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+    if (!token) return res.status(400).send({ message: "Token bulunamadı!" });
+    await User.updateOne({ _id: user._id, verified: true });
+    await token.remove();
+    res.status(200).send({ message: "Email doğrulandı!" });
+  } catch (error) {
     res.status(500).json(err);
   }
 });
